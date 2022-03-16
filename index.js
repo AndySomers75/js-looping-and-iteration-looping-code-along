233a153d7a3951a1dc18a36e8ce4b4525c56beab
Andrew <andrew@pureaquasprings.com>
2022-03-16 13:01:13 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const namesArray = ["Lisa", "Kaitlin", "Jan"];
+
+function writeCards(namesArray) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++) {
+    thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`); 
+     
+}
+return thankYouCards
+}
+
+function countDown( startingNumber = 10 ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
